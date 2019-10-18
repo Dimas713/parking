3923e5afb6a8ba43380f2c080ddcc2cc7248c001
dimas <46415558+Dimas713@users.noreply.github.com>
2019-10-17 22:15:56 -0500

    added parkingLot class

--- parking/src/parking/Main.java
+++ parking/src/parking/Main.java
@@ -1,9 +1,26 @@
 package parking;
 
+class ParkingLot
+{
+    public int    Total_Profit; 
+    public int    Cost_Per_Car;
+    public int    Max_Capacity; 
+    public boolean full;
+ };
+ 
+ class Car
+ {
+	 public boolean is_Parked; 
+	 
+  };
+
 public class Main {
 
 	public static void main(String[] args) {
-		int capacity = 100;
+	
+		
+		
+		System.out.println("end of program..");
 
 	}
 
