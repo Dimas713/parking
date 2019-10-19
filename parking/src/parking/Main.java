4a834b6011e029e2b116bc005d0e354739f83a36
dimas <46415558+Dimas713@users.noreply.github.com>
2019-10-19 06:04:12 -0500

    Expanded on  the parkinglot class and the Car class

--- parking/src/parking/Main.java
+++ parking/src/parking/Main.java
@@ -1,23 +1,103 @@
 package parking;
+import java.util.LinkedList; 
+import java.util.Queue;
+import java.util.Random;
 
 class ParkingLot
 {
-    public int    Total_Profit; 
-    public int    Cost_Per_Car;
-    public int    Max_Capacity; 
-    public boolean full;
+    private int    Total_Profit; 
+    private int    Cost_Per_HR;
+    private boolean full;
+    public int capacity = 100;
+    Car[] space = new Car[capacity];
+    
+    public ParkingLot()
+    {
+    	for(int i=0; i < capacity; i++)
+    	{
+    		space[i] = null;
+    	}
+    }
+    
+    
+    public boolean is_full()
+    {
+    	for(int i=0; i < capacity; i++)
+    	{
+    		if(space[i] == null)
+    		{
+    			full = false;
+    			break;
+    		}
+    		else
+    		{
+    			full = true;
+    		}
+    	}
+    	return full;
+    }
+    
  };
  
- class Car
+ class Car extends ParkingLot
  {
-	 public boolean is_Parked; 
+	 private boolean is_Parked; 
+	 private double time_In;
+	 private double time_out;
+	 private double time_parked;
 	 
+	public Car(double wait_time)
+	 {
+		time_parked = wait_time ;
+	 }
+	
+	
   };
 
 public class Main {
 
 	public static void main(String[] args) {
+		// create instance of Random class 
+        Random rand = new Random(); 
 	
+		System.out.println("Creating Parking Lot...");
+		ParkingLot LotA = new ParkingLot();
+	    Queue<Car> entrance = new LinkedList<>();
+	    
+	    int num_of_Cars = 100;
+	 
+	    
+	    //Adds car objects to the entrance  queue
+	    for (int i=0; i< num_of_Cars; i++) 
+	    {
+	    	
+	    	entrance.add(new Car(rand.nextInt(25)));
+	    		
+	    }
+	    System.out.println("There are " + num_of_Cars + " cars in the entrence queue.");
+	    
+	    //lets the first batch of cars into the parking lot if entrance queue is empty or there is
+	    //no more space no more cars will go in 
+	    int counter= 0;
+	    while(LotA.is_full() == false)
+	    {
+	    	if(LotA.space[counter] == null && counter < LotA.capacity && entrance.peek() != null)
+	    	{
+	    		LotA.space[counter] = entrance.peek();
+	    		entrance.remove();
+	    		counter++;
+	    	}
+	    	else
+	    	{
+	    		counter++;
+	    	}
+	    	
+	    	if(entrance.peek() == null)
+	    	{
+	    		break;
+	    	}
+	    	
+	    }
 		
 		
 		System.out.println("end of program..");
