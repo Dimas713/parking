ca26d39c344d87438bfcf0bc749909cd906267ec
dimas <46415558+Dimas713@users.noreply.github.com>
2019-10-27 21:46:42 -0500

    added additional parking lot objects and classes

--- parking/src/parking/Main.java
+++ parking/src/parking/Main.java
@@ -6,22 +6,71 @@ import java.util.Queue;
 import java.util.Random;
 import java.util.Scanner;
 
-class ParkingLot
+class group
+{
+	public int Cost_Per_HR[] = {5 ,7 ,10 } ;
+	
+	public int Num_of_groups()
+	{
+		return Cost_Per_HR.length ;
+	}
+	
+	
+	public int Price_Inquiry(int Lotnum)
+	{
+		if(Lotnum < Cost_Per_HR.length && Lotnum > 0)
+		{
+			--Lotnum;
+			return Cost_Per_HR[Lotnum];
+		}
+		else
+		{
+			return -1;
+		}
+		
+		
+	}
+};
+
+class ParkingLot extends group
 {
     public int    Total_Profit; 
-    public int    Cost_Per_HR = 10;
     private boolean full;
     private boolean empty;
-    public int capacity = 100;
+    private int capacity = 100;
+    public int cost ;
     Car[] space = new Car[capacity];
+    Queue<Car> entrance = new LinkedList<>();
+    Queue<Car> exit = new LinkedList<>();
     
-    public ParkingLot()
+    public ParkingLot(int groupnum)
     {
+    	cost = Cost_Per_HR[groupnum-1];
     	Total_Profit = 0;
     	for(int i=0; i < capacity; i++)
     	{
     		space[i] = null;
     	}
+    }
+    
+    public void carEnter(int count)
+    {
+    	Random rand = new Random(); 
+    	int num_of_Cars = count ;
+    	
+    	//Adds car objects to the entrance  queue
+	    for (int i=0; i< num_of_Cars; i++) 
+	    {
+	    	
+	    	entrance.add(new Car(rand.nextInt(25)));
+	    		
+	    }
+    }
+    
+    public int parkingSize()
+    {
+    	int size = capacity;
+    	return size;
     }
     
     
@@ -53,7 +102,7 @@ class ParkingLot
     		}
     		else
     		{
-    			full = false;
+    			empty = false;
     			break;
     		}
     	}
@@ -62,18 +111,22 @@ class ParkingLot
     
  };
  
- class Car extends ParkingLot
+ class Car
  {
 	 private boolean is_Parked; 
 	 private double time_In;
 	 private double time_out;
-	 public int time_parked;
+	 private int time_parked;
 	 
 	public Car(int wait_time)
 	 {
 		time_parked = wait_time ;
 	 }
 	
+	public int timeParked()
+	{
+		return time_parked; 
+	}
 	
   };
   
@@ -81,42 +134,109 @@ class ParkingLot
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException 
+	{
 		int time = 0;
-		// create instance of Random class 
-        Random rand = new Random(); 
-	
-		System.out.println("Creating Parking Lot...");
-		ParkingLot LotA = new ParkingLot();
-	    Queue<Car> entrance = new LinkedList<>();
-	    Queue<Car> exit = new LinkedList<>();
+	 
+		System.out.println("Creating Parking Lots..." );
+		ParkingLot LotA = new ParkingLot(1);
+		ParkingLot LotB = new ParkingLot(2);
+		ParkingLot LotC = new ParkingLot(3);
 	    
 	  
-	    
 	      Scanner sc = new Scanner(new File("test1.txt"));
 	      int num = sc.nextInt();
 	      int num_of_Cars = num;
+	    /*
+	    Scanner myObj = new Scanner(System.in);
+	    System.out.println("Enter number of cars");
+	    int num_of_Cars = myObj.nextInt();;
+	 */
+	    int lotnum = 1;
 	    
-	 
-	    
-	    //Adds car objects to the entrance  queue
-	    for (int i=0; i< num_of_Cars; i++) 
+	    System.out.println("There are " + num_of_Cars + " cars in the entrence queue for lot " );
+	    while(num_of_Cars > 0)
 	    {
-	    	
-	    	entrance.add(new Car(rand.nextInt(25)));
+	    // The cars enter the entrance queue based on lowest price
+	    if((LotA.cost < LotB.cost && LotA.cost < LotC.cost))
+	    {
+	    	if(num_of_Cars < LotA.parkingSize())
+	    	{
+	    		LotA.carEnter(num_of_Cars) ;
+	    		num_of_Cars = 0;
+	    	}
+	    	else
+	    	{
+	    		LotA.carEnter(LotA.parkingSize());
+	    		num_of_Cars = num_of_Cars - LotA.parkingSize() ;
+	    		if(LotB.cost < LotC.cost)
+	    		{
+	    			if(num_of_Cars < LotB.parkingSize())
+	    	    	{
+	    	    		LotB.carEnter(num_of_Cars) ;
+	    	    		num_of_Cars = 0;
+	    	    	}
+	    			else
+	    			{
+	    				LotB.carEnter(LotB.parkingSize());
+	    	    		num_of_Cars = num_of_Cars - LotB.parkingSize() ;
+	    			}
+	    		}
+	    		else
+	    		{
+	    			if(num_of_Cars < LotC.parkingSize())
+	    	    	{
+	    	    		LotC.carEnter(num_of_Cars) ;
+	    	    		num_of_Cars = 0;
+	    	    	}
+	    			else
+	    			{
+	    				LotC.carEnter(LotC.parkingSize());
+	    	    		num_of_Cars = num_of_Cars - LotC.parkingSize() ;
+	    			}
+	    		}
 	    		
+	    	}
 	    }
-	    System.out.println("There are " + num_of_Cars + " cars in the entrence queue.");
-	    
-	    //lets the first batch of cars into the parking lot if entrance queue is empty or there is
+	    else if(LotB.cost < LotA.cost && LotB.cost < LotA.cost)
+	    {
+	    	if(num_of_Cars < LotB.parkingSize())
+	    	{
+	    		LotB.carEnter(num_of_Cars) ;
+	    		num_of_Cars = 0;
+	    	}
+	    	else
+	    	{
+	    		LotB.carEnter(LotB.parkingSize());
+	    		num_of_Cars = num_of_Cars - LotB.parkingSize() ;
+	    	}
+	    }
+	    else if(LotC.cost < LotA.cost && LotC.cost < LotB.cost)
+	    {
+	    	if(num_of_Cars < LotC.parkingSize())
+	    	{
+	    		LotC.carEnter(num_of_Cars) ;
+	    		num_of_Cars = 0;
+	    	}
+	    	else
+	    	{
+	    		LotC.carEnter(LotC.parkingSize());
+	    		num_of_Cars = num_of_Cars - LotC.parkingSize() ;
+	    }
+	    }
+	    }
+	    	
+	   
+	    //lets the first batch of cars into the parking lot A if entrance queue is empty or there is
 	    //no more space no more cars will go in 
+	    int num_of_Carsin_A = LotA.entrance.size();
 	    int counter= 0;
 	    while(LotA.is_full() == false)
 	    {
-	    	if(LotA.space[counter] == null && counter < LotA.capacity && entrance.peek() != null)
+	    	if(LotA.space[counter] == null && counter < LotA.parkingSize() && LotA.entrance.peek() != null)
 	    	{
-	    		LotA.space[counter] = entrance.peek();
-	    		entrance.remove();
+	    		LotA.space[counter] = LotA.entrance.peek();
+	    		LotA.entrance.remove();
 	    		counter++;
 	    	}
 	    	else
@@ -124,21 +244,20 @@ public class Main {
 	    		counter++;
 	    	}
 	    	
-	    	if(entrance.peek() == null)
+	    	if(LotA.entrance.peek() == null)
 	    	{
 	    		break;
 	    	}
 	    	
 	    }
 	    
-	    System.out.println("The parking lot will start recording profit at hour 0");
 	    
 	   //bubble sort car array so i can have the timeParked at the beginning and 
 	    //cars that will be parked longer at the end
 	    int n;
-	    if(num_of_Cars < LotA.capacity)
+	    if(num_of_Carsin_A < LotA.parkingSize())
 	    {
-	    	 n = num_of_Cars;
+	    	 n = num_of_Carsin_A;
 	    }
 	    else
 	    {
@@ -147,32 +266,33 @@ public class Main {
 	  
 	        for (int i = 0; i < n-1; i++) 
 	            for (int j = 0; j < n-i-1; j++) 
-	                if (LotA.space[j].time_parked > LotA.space[j+1].time_parked) 
+	                if (LotA.space[j].timeParked() > LotA.space[j+1].timeParked()) 
 	                { 
 	                    // swap LotA.space[j+1] and LotA.space[i] 
 	                    Car temp = LotA.space[j]; 
 	                    LotA.space[j] = LotA.space[j+1]; 
 	                    LotA.space[j+1] = temp; 
 	                } 
-	    
+	        
 	        //simulates a car coming into the lot from the entrance queue
 	        //once the car is done being parked it will go to the exit queue
 	        while(!LotA.is_empty())
 	        {
-	        	if(entrance.peek() != null)
+	        	
+	        	if(LotA.entrance.peek() != null)
 	        	{
 	        		if(LotA.space[0] != null)
 	        		{
-	        			time = time + (LotA.space[0].time_parked - time);
-	        			exit.add(LotA.space[0]);
+	        			time = time + (LotA.space[0].timeParked() - time);
+	        			LotA.exit.add(LotA.space[0]);
 	        			
-	        			LotA.space[0] = entrance.peek();
-	        			entrance.remove();
+	        			LotA.space[0] = LotA.entrance.peek();
+	        			LotA.entrance.remove();
 	        			
 	        			
 	        			//bubble sort car array so i can have the timeParked at the beginning and 
 	        		    //cars that will be parked longer at the end
-	        			if(num_of_Cars < LotA.capacity)
+	        			if(num_of_Cars < LotA.parkingSize())
 	        		    {
 	        		    	 n = num_of_Cars;
 	        		    }
@@ -183,7 +303,7 @@ public class Main {
 	        		  
 	        		        for (int i = 0; i < n-1; i++) 
 	        		            for (int j = 0; j < n-i-1; j++) 
-	        		                if (LotA.space[j].time_parked > LotA.space[j+1].time_parked) 
+	        		                if (LotA.space[j].timeParked() > LotA.space[j+1].timeParked()) 
 	        		                { 
 	        		                    // swap LotA.space[j+1] and LotA.space[i] 
 	        		                    Car temp = LotA.space[j]; 
@@ -196,9 +316,10 @@ public class Main {
 	        	}
 	        	else
 	        	{
-	        		if(num_of_Cars < LotA.capacity)
+	        		
+	        		if(num_of_Carsin_A < LotA.parkingSize())
 	        		{
-	        			n = num_of_Cars;
+	        			n = num_of_Carsin_A;
 	        		}
 	        		else
 	        		{
@@ -206,8 +327,8 @@ public class Main {
 	        		}
 	        		for(int i =0; i < n; i++)
 	        		{
-	        			time = time + (LotA.space[i].time_parked - time);
-	        			exit.add(LotA.space[i]);
+	        			time = time + (LotA.space[i].timeParked() - time);
+	        			LotA.exit.add(LotA.space[i]);
 	        			LotA.space[i] = null;
 	        		}
 	        	}
@@ -216,17 +337,269 @@ public class Main {
 	        
 	        //eventually the parking lot will be empty and you can process the exit queue
 	        //will add the amount of hr each car stayed to calculate the profit
-		while(exit.peek() != null)
+		while(LotA.exit.peek() != null)
 		{
-			LotA.Total_Profit = LotA.Total_Profit + (exit.peek().time_parked * LotA.Cost_Per_HR);
-			exit.remove();
+			LotA.Total_Profit = LotA.Total_Profit + (LotA.exit.peek().timeParked() * LotA.cost);
+			LotA.exit.remove();
 		}
 		
 		System.out.println("It took " + time + " hours to make " + LotA.Total_Profit + " dollars at "+
-							 LotA.Cost_Per_HR + " per hour with a traffic of "+  num_of_Cars + " cars");
+							 LotA.cost + " dollards per hour for lot # " + lotnum) ;
+		lotnum++;
+		//-----------------------------------------------------------------------------------------
+		// start of lot B
+		
+		//lets the first batch of cars into the parking lot A if entrance queue is empty or there is
+	    //no more space no more cars will go in 
+	    int num_of_Carsin_B = LotB.entrance.size();
+	     counter= 0;
+	    
+	    while(LotB.is_full() == false)
+	    {
+	    	if(LotB.space[counter] == null && counter < LotB.parkingSize() && LotB.entrance.peek() != null)
+	    	{
+	    		LotB.space[counter] = LotB.entrance.peek();
+	    		LotB.entrance.remove();
+	    		counter++;
+	    	}
+	    	else
+	    	{
+	    		counter++;
+	    	}
+	    	
+	    	if(LotB.entrance.peek() == null)
+	    	{
+	    		break;
+	    	}
+	    	
+	    }
+	    
+	    
+	   //bubble sort car array so i can have the timeParked at the beginning and 
+	    //cars that will be parked longer at the end
+	    
+	    if(num_of_Carsin_B < LotB.parkingSize())
+	    {
+	    	 n = num_of_Carsin_B;
+	    }
+	    else
+	    {
+	    	n = LotB.space.length;
+	    }
+	  
+	        for (int i = 0; i < n-1; i++) 
+	            for (int j = 0; j < n-i-1; j++) 
+	                if (LotB.space[j].timeParked() > LotB.space[j+1].timeParked()) 
+	                { 
+	                    // swap LotA.space[j+1] and LotA.space[i] 
+	                    Car temp = LotB.space[j]; 
+	                    LotB.space[j] = LotB.space[j+1]; 
+	                    LotB.space[j+1] = temp; 
+	                } 
+	        
+	        //simulates a car coming into the lot from the entrance queue
+	        //once the car is done being parked it will go to the exit queu
+	        
+	        while(!LotB.is_empty())
+	        {
+	        	
+	        	if(LotB.entrance.peek() != null)
+	        	{
+	        		if(LotB.space[0] != null)
+	        		{
+	        			time = time + (LotB.space[0].timeParked() - time);
+	        			LotB.exit.add(LotB.space[0]);
+	        			
+	        			LotB.space[0] = LotB.entrance.peek();
+	        			LotB.entrance.remove();
+	        			
+	        			
+	        			//bubble sort car array so i can have the timeParked at the beginning and 
+	        		    //cars that will be parked longer at the end
+	        			if(num_of_Cars < LotB.parkingSize())
+	        		    {
+	        		    	 n = num_of_Cars;
+	        		    }
+	        		    else
+	        		    {
+	        		    	n = LotB.space.length;
+	        		    }
+	        		  
+	        		        for (int i = 0; i < n-1; i++) 
+	        		            for (int j = 0; j < n-i-1; j++) 
+	        		                if (LotB.space[j].timeParked() > LotB.space[j+1].timeParked()) 
+	        		                { 
+	        		                    // swap LotA.space[j+1] and LotA.space[i] 
+	        		                    Car temp = LotB.space[j]; 
+	        		                    LotB.space[j] = LotB.space[j+1]; 
+	        		                    LotB.space[j+1] = temp; 
+	        		                } 	
+	        			
+	        			
+	        		}
+	        	}
+	        	else
+	        	{
+	        		
+	        		if(num_of_Carsin_B < LotB.parkingSize())
+	        		{
+	        			n = num_of_Carsin_B;
+	        		}
+	        		else
+	        		{
+	        			n = LotB.space.length;
+	        		}
+	        		for(int i =0; i < n; i++)
+	        		{
+	        			time = time + (LotB.space[i].timeParked() - time);
+	        			LotB.exit.add(LotB.space[i]);
+	        			LotB.space[i] = null;
+	        		}
+	        	}
+	        	
+	        }
+	        
+	        //eventually the parking lot will be empty and you can process the exit queue
+	        //will add the amount of hr each car stayed to calculate the profit
+		while(LotB.exit.peek() != null)
+		{
+			LotB.Total_Profit = LotB.Total_Profit + (LotB.exit.peek().timeParked() * LotB.cost);
+			LotB.exit.remove();
+		}
+		
+		System.out.println("It took " + time + " hours to make " + LotB.Total_Profit + " dollars at "+
+							 LotB.cost + " dollards per hour for lot # " + lotnum) ;
+		lotnum++;
+		
+		//-----------------------------------------------------------------------------------------
+				// start of lot c
+				
+				//lets the first batch of cars into the parking lot A if entrance queue is empty or there is
+			    //no more space no more cars will go in 
+			    int num_of_Carsin_C = LotC.entrance.size();
+			     counter= 0;
+			    
+			    while(LotC.is_full() == false)
+			    {
+			    	if(LotC.space[counter] == null && counter < LotC.parkingSize() && LotC.entrance.peek() != null)
+			    	{
+			    		LotC.space[counter] = LotC.entrance.peek();
+			    		LotC.entrance.remove();
+			    		counter++;
+			    	}
+			    	else
+			    	{
+			    		counter++;
+			    	}
+			    	
+			    	if(LotC.entrance.peek() == null)
+			    	{
+			    		break;
+			    	}
+			    	
+			    }
+			    
+			    
+			   //bubble sort car array so i can have the timeParked at the beginning and 
+			    //cars that will be parked longer at the end
+			    
+			    if(num_of_Carsin_C < LotC.parkingSize())
+			    {
+			    	 n = num_of_Carsin_C;
+			    }
+			    else
+			    {
+			    	n = LotC.space.length;
+			    }
+			  
+			        for (int i = 0; i < n-1; i++) 
+			            for (int j = 0; j < n-i-1; j++) 
+			                if (LotC.space[j].timeParked() > LotC.space[j+1].timeParked()) 
+			                { 
+			                    // swap LotC.space[j+1] and LotC.space[i] 
+			                    Car temp = LotC.space[j]; 
+			                    LotC.space[j] = LotC.space[j+1]; 
+			                    LotC.space[j+1] = temp; 
+			                } 
+			        
+			        //simulates a car coming into the lot from the entrance queue
+			        //once the car is done being parked it will go to the exit queue
+			        
+			        while(!LotC.is_empty())
+			        {
+			        	
+			        	if(LotC.entrance.peek() != null)
+			        	{
+			        		if(LotC.space[0] != null)
+			        		{
+			        			time = time + (LotC.space[0].timeParked() - time);
+			        			LotC.exit.add(LotC.space[0]);
+			        			
+			        			LotC.space[0] = LotC.entrance.peek();
+			        			LotC.entrance.remove();
+			        			
+			        			
+			        			//bubble sort car array so i can have the timeParked at the beginning and 
+			        		    //cars that will be parked longer at the end
+			        			if(num_of_Cars < LotC.parkingSize())
+			        		    {
+			        		    	 n = num_of_Cars;
+			        		    }
+			        		    else
+			        		    {
+			        		    	n = LotC.space.length;
+			        		    }
+			        		  
+			        		        for (int i = 0; i < n-1; i++) 
+			        		            for (int j = 0; j < n-i-1; j++) 
+			        		                if (LotC.space[j].timeParked() > LotC.space[j+1].timeParked()) 
+			        		                { 
+			        		                    // swap LotA.space[j+1] and LotA.space[i] 
+			        		                    Car temp = LotC.space[j]; 
+			        		                    LotC.space[j] = LotC.space[j+1]; 
+			        		                    LotC.space[j+1] = temp; 
+			        		                } 	
+			        			
+			        			
+			        		}
+			        	}
+			        	else
+			        	{
+			        		
+			        		if(num_of_Carsin_C < LotC.parkingSize())
+			        		{
+			        			n = num_of_Carsin_C;
+			        		}
+			        		else
+			        		{
+			        			n = LotC.space.length;
+			        		}
+			        		for(int i =0; i < n; i++)
+			        		{
+			        			time = time + (LotC.space[i].timeParked() - time);
+			        			LotC.exit.add(LotC.space[i]);
+			        			LotC.space[i] = null;
+			        		}
+			        	}
+			        	
+			        }
+			        
+			        //eventually the parking lot will be empty and you can process the exit queue
+			        //will add the amount of hr each car stayed to calculate the profit
+				while(LotC.exit.peek() != null)
+				{
+					LotC.Total_Profit = LotC.Total_Profit + (LotC.exit.peek().timeParked() * LotC.cost);
+					LotC.exit.remove();
+				}
+				
+				System.out.println("It took " + time + " hours to make " + LotC.Total_Profit + " dollars at "+
+									 LotC.cost + " dollards per hour for lot # " + lotnum) ;
+				lotnum++;
 		
 		System.out.println("end of program..");
 
 	}
 
-}
+
+	}
+	
